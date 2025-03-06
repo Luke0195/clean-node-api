e133fbf3054cf422f38b809f60bfa87c92460f24
Lucas Santos <lucas201273@gmail.com>
2025-03-06 08:23:48 -0300

    test: ensure SignUpController calls EmailValidator with correct e-mail

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -93,4 +93,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct e-mail', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const emailValidatorSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(emailValidatorSpy).toHaveBeenCalledWith('any_mail@mail.com')
+  })
 })
