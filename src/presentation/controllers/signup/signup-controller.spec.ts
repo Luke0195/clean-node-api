1497d6b0f7ab28c381e74c33cb15a716e80e6e7e
Lucas Santos <lucas201273@gmail.com>
2025-03-09 10:02:44 -0300

    refactor: make server error returns the error stack

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -159,7 +159,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Internal Server Error'))
   })
 
   test('Should calls AddAccount with correct values', async () => {
@@ -197,7 +197,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Internal Server Error'))
   })
 
   test('Should returns 201 when valid data is provided', async () => {
